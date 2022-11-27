54c62a9d514ec7414534a5686d87f5e7faa4062e
Denisssska <yarmoshkoden18m@gmail.com>
2022-11-27 16:14:31 +0300

    create calendar

---
+++ src/utils/createDate.ts
@@ -0,0 +1,37 @@
+import {getWeekNumber} from "./getWeekNumber";
+
+export interface CreateParams {
+    locale?: string;
+    date?: Date;
+}
+
+export const createDate = (params?: CreateParams) => {
+    const locale = params?.locale ?? 'default';
+    const d = params?.date ?? new Date();
+    const dayNumber = d.getDate();
+    const day = d.toLocaleDateString(locale, {weekday: 'long'});
+    const dayShort = d.toLocaleDateString(locale, {weekday: 'short'});
+    const dayNumberInWeek = d.getDate() + 1;
+    const year = d.getFullYear();
+    const yearShort = d.toLocaleDateString(locale, {year: '2-digit'});
+    const month = d.toLocaleDateString(locale, {month: 'long'});
+    const monthShort = d.toLocaleDateString(locale, {month: 'short'});
+    const monthNumber = d.getMonth() + 1;
+    const monthIndex = d.getMonth();
+    const timeStamp = d.getTime();
+    const week = getWeekNumber(d)
+    return {
+        dayNumber,
+        day,
+        dayShort,
+        dayNumberInWeek,
+        year,
+        yearShort,
+        month,
+        monthShort,
+        monthNumber,
+        monthIndex,
+        timeStamp,
+        week
+    }
+}
