5045e5bef5e8639debb7953d2d55ecd561bd867f
Denisssska <yarmoshkoden18m@gmail.com>
2022-11-27 13:02:08 +0300

    add lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {lazy, Suspense} from 'react';
+
+import {NavLink, Route, Routes} from "react-router-dom";
+//lazy -Это синтаксис для ленивой загрузки компонентов в React. Вы заметите, что он внутренне использует dynamic import() syntax, который webpack использует как «точку разделения».
+//Компонент <Suspense> отобразит <div> Loading ... </div>, ожидая динамической загрузки кода маршрута. Итак, наш последний компонент App.tsx в итоге выглядит так:
+const About = lazy(() => import('./About'));
+const Home = lazy(() => import('./Home'));
+
+interface RoutesProps {
+    children?: React.ReactNode;
+    location?: Partial<Location> | string;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Routes>
+            </Suspense>
+        </>
+    );
 }
 
 export default App;
